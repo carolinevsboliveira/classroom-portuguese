4fac1036ba5178d0963d43315dddd286b7a9d868
carolinevsboliveira <carolinevsboliveira@gmail.com>
2022-03-25 22:15:29 -0700

    fix: fixed missmatch between names

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -3,13 +3,12 @@ import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.REACT_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2021-11-16',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN
+  token: process.env.REACT_SANITY_API_TOKEN
 });
-
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source: SanityImageSource) => builder.image(source);
