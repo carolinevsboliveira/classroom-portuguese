23c6f8fef97a08a9f088b69df9be1152f797bf33
carolinevsboliveira <carolinevsboliveira@gmail.com>
2022-04-11 21:09:18 -0400

    fix: fixed routes to prevent wrong redirect when user search for classes

--- frontend/pages/classes-registration.tsx
+++ frontend/pages/classes-registration.tsx
@@ -1,10 +1,8 @@
 import React, { useState, Fragment } from 'react';
 import { BackdropWithLoader, ClassForm, Toast } from '../src/components';
 import { useAuth } from '../src/contexts';
-import { useRouter } from 'next/router';
 import { useValidUser } from '../src/hooks';
 const Classes = () => {
-  const { push } = useRouter();
   const [error, setError] = useState({ state: false, message: '' });
   const { currentUser } = useAuth();
 
--- frontend/pages/classes/index.tsx
+++ frontend/pages/classes/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { ClassList } from '../src/components';
 import { useQuery } from 'react-query';
-import { client } from '../src/client';
-import { classesQuery } from '../src/utils';
-const ClassFeed = () => {
+import { client } from '../../src/client';
+import { ClassList } from '../../src/components';
+import { classesQuery } from '../../src/utils';
+
+export const ClassPage = () => {
   const fetchClasses = async () => {
     return await client.fetch(classesQuery);
   };
@@ -11,5 +12,4 @@ const ClassFeed = () => {
   const { isLoading, data, isFetched } = useQuery('classes', fetchClasses);
   return <ClassList isLoading={isLoading} data={data} isFetched={isFetched} />;
 };
-
-export default ClassFeed;
+export default ClassPage;
