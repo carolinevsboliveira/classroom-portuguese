5b1e2f4fa42e90bb950b1266ee19de76cca8884c
carolinevsboliveira <carolinevsboliveira@gmail.com>
2022-04-21 00:34:34 -0400

    chore: typed query response to sanity

---
+++ frontend/src/shared-interfaces/class-response.ts
@@ -0,0 +1,16 @@
+export default interface ClassResponse {
+  description: string;
+  duration: number;
+  file: any;
+  image: any;
+  link: string;
+  subtitle: string;
+  teacher: any;
+  time: string;
+  title: string;
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+}
--- frontend/src/utils/sanity-querys.ts
+++ frontend/src/utils/sanity-querys.ts
@@ -1,8 +1,10 @@
 export const teachers = `*[_type == 'user' && isInstructor == true]`;
+
 export const currentTeacherUserName = (id: string) =>
   `*[_type == 'user' && _id == '${id}'][0]{userName}`;
 
 export const classesQuery = `*[_type == 'classroom']`;
+
 export const fileUrlQuery = (classroomId: string) => {
   return `*[_type == "classroom" && _id == '${classroomId}']{
       file{
@@ -13,5 +15,5 @@ export const fileUrlQuery = (classroomId: string) => {
     }`;
 };
 
-export const currentClass = (id: string) =>
-  `*[_type == 'classroom' && _id == '${id}'][0]`;
+export const currentClass = (key: string) =>
+  `*[_type == 'classroom' && key == '${key}'][0]`;
